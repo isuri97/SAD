8f1d8ab13c8cebd3825f522126867b15fa7f3334
isuri97 <you@example.com>
2023-07-04 17:16:25 +0530

    change in code

--- model/model.py
+++ model/model.py
@@ -10,17 +10,12 @@ import logging
 # transformers_logger.setLevel(logging.WARNING)
 
 # Load data
-train_df = pd.read_csv('train-dataset.csv')
-val_df = pd.read_csv('val-dataset.csv')
+train_df = pd.read_csv('train-dataset.csv',sep=",")
+# val_df = pd.read_csv('val-dataset.csv')
 
-train_df = train_df[['text', 'label']]
-# train_df['labels'] = encode(train_df["label"])
-
-# train_df.columns = ["text", "labels"]
+train_df = train_df[['text', 'labels']]
+# train_df['label'] = encode(train_df['label'])
 print(train_df)
-# val_df = val_df[['text', 'label']]
-
-# model_args = ClassificationArgs(num_train_epochs=1)
 
 # Optional model configuration
 train_set, validation_set = train_test_split(train_df, test_size=0.2)
@@ -30,17 +25,19 @@ train_set, validation_set = train_test_split(train_df, test_size=0.2)
 train_args = {"reprocess_input_data": True,
              "overwrite_output_dir": True,
              "fp16":False,
-             "num_train_epochs": 4}
+             "num_train_epochs": 4,
+             "train_batch_size": 16
+                           }
 
 # Create a ClassificationModel
 model = ClassificationModel(
     "bert", "bert-base-cased",
-    num_labels=4,
-    args=train_args
+    args=train_args,
+
 )
 
 # Train the model
-model.train_model(train_set)
-
+model.train_model(train_df)
+#
 # Evaluate the model
 result, model_outputs, wrong_predictions = model.eval_model(validation_set)
