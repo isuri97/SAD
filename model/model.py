5458fd2e72b3a705f9277e08f391d1208723b01c
isuri97 <you@example.com>
2023-07-05 05:52:02 +0530

    change in the code

--- model/model.py
+++ model/model.py
@@ -34,7 +34,7 @@ test_sentences = validation_set['text'].tolist()
 train_args = {"reprocess_input_data": True,
              "overwrite_output_dir": True,
              "fp16":False,
-             "num_train_epochs": 4,
+             "num_train_epochs": 3,
              "train_batch_size": 8,
              "use_multiprocessing": False,
              "use_multiprocessing_for_evaluation":False,
