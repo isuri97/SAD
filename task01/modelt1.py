0f728dff14441d909197743ada3e5b0411dc5d69
isuri97 <you@example.com>
2023-07-07 23:31:08 +0530

    adding wandb

--- task01/modelt1.py
+++ task01/modelt1.py
@@ -50,7 +50,6 @@ train_args = {"reprocess_input_data": True,
              "use_multiprocessing": False,
              "use_multiprocessing_for_evaluation":False,
              "n_fold":1,
-             "learning_rate": 2e-4
         }
 
 
